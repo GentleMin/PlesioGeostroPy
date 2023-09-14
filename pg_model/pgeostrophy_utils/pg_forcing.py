58f1f6d0200293241bdc529f7c394c5f798dc91c
gentlemin <40021781+GentleMin@users.noreply.github.com>
2023-09-14 11:04:48 +0200

    Renaming

--- pg_model/pgeostrophy_utils/pg_forcing.py
+++ pg_model/pgeostrophy_utils/pg_forcing.py
@@ -8,6 +8,7 @@ import sympy
 from sympy import diff
 from sympy import Derivative as diff_u
 from .pg_fields import *
+from .base_utils import linearize
 
 
 """Lorentz force"""
@@ -33,27 +34,19 @@ Le_p_expr = Bs_e*diff(Bp_e, s) + 1/s*Bp_e*diff(Bp_e, p) + Bz_e*dBp_dz_e + 1/s*Bs
 # Linearized form of Lorentz force in the equatorial plane $L_{e\phi}$
 # $L_{e\phi}$ is quadratic in the magnetic field components in the equatorial plane.
 # Linearized form involves cross terms between background and perturbational magnetic fields.
-Le_p_perturbed = Le_p_expr.subs(linearization_subs_map)
-Le_p_perturbed = sympy.collect(Le_p_perturbed.simplify().expand(), eps)
-Le_p_lin = Le_p_perturbed.coeff(eps, 1)
+Le_p_lin = linearize(Le_p_expr, linearization_subs_map, perturb_var=eps)
 
 # For the integrated quantities, the Lorentz force IS a linear function of magnetic moments. Essentially no linearization required.
 # However, the boundary terms and the equatorial terms are quadratic in magnetic fields. These terms need to be linearized.
 
 # Linearized form for $\overline{L_s}$
-Ls_sym_perturbed = Ls_sym_expr.subs(linearization_subs_map)
-Ls_sym_perturbed = sympy.collect(Ls_sym_perturbed.simplify().expand(), eps)
-Ls_sym_lin = Ls_sym_perturbed.coeff(eps, 1)
+Ls_sym_lin = linearize(Ls_sym_expr, linearization_subs_map, perturb_var=eps)
 
 # Linearized form for $\overline{L_\phi}$
-Lp_sym_perturbed = Lp_sym_expr.subs(linearization_subs_map)
-Lp_sym_perturbed = sympy.collect(Lp_sym_perturbed.simplify().expand(), eps)
-Lp_sym_lin = Lp_sym_perturbed.coeff(eps, 1)
+Lp_sym_lin = linearize(Lp_sym_expr, linearization_subs_map, perturb_var=eps)
 
 # Linearized form for $\widetilde{L_z}$
-Lz_asym_perturbed = Lz_asym_expr.subs(linearization_subs_map)
-Lz_asym_perturbed = sympy.collect(Lz_asym_perturbed.simplify().expand(), eps)
-Lz_asym_lin = Lz_asym_perturbed.coeff(eps, 1)
+Lz_asym_lin = linearize(Lz_asym_expr, linearization_subs_map, perturb_var=eps)
 
 # Curl of horizontal components $\nabla \times \mathbf{L}_e$
 # Curl is linear, linearize (curl (field)) = curl (linearize (field))
