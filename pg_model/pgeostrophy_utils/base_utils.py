b0f7002538985daebc710c7964624a5e1f5ae2af
gentlemin <40021781+GentleMin@users.noreply.github.com>
2023-09-14 11:04:48 +0200

    Renaming

--- pg_model/pgeostrophy_utils/base_utils.py
+++ pg_model/pgeostrophy_utils/base_utils.py
@@ -6,17 +6,17 @@ Jingtao Min @ ETH-EPM, 09.2023
 
 import sympy
 from ..sympy_supp import vector_calculus_3d as v3d
-from .pg_fields import H, H_s, s, p, z, cyl_op
+from .pg_fields import H, H_s, s, p, z, cyl_op, eps
 
 
-def int_sym(field):
+def integrate_sym(field):
     """equatorially symmetric / even integral
     Note: input field has to be a function of cylindrical coordinates
     """
     return sympy.integrate(field, (z, -H, H))
 
 
-def int_asym(field):
+def integrate_asym(field):
     """equatorially asymmetric / odd integral
     Note: input field has to be a function of cylindrical coordinates
     """
@@ -35,14 +35,21 @@ def field_to_moment(B_field):
     )
     """
     assert len(B_field) == 3
-    Mss = int_sym(B_field[0]*B_field[0])
-    Mpp = int_sym(B_field[1]*B_field[1])
-    Msp = int_sym(B_field[0]*B_field[1])
-    Msz = int_asym(B_field[0]*B_field[2])
-    Mpz = int_asym(B_field[1]*B_field[2])
-    zMss = int_asym(z*B_field[0]*B_field[0])
-    zMpp = int_asym(z*B_field[1]*B_field[1])
-    zMsp = int_asym(z*B_field[0]*B_field[1])
+    Mss = integrate_sym(B_field[0]*B_field[0])
+    Mpp = integrate_sym(B_field[1]*B_field[1])
+    Msp = integrate_sym(B_field[0]*B_field[1])
+    Msz = integrate_asym(B_field[0]*B_field[2])
+    Mpz = integrate_asym(B_field[1]*B_field[2])
+    zMss = integrate_asym(z*B_field[0]*B_field[0])
+    zMpp = integrate_asym(z*B_field[1]*B_field[1])
+    zMsp = integrate_asym(z*B_field[0]*B_field[1])
     return Mss, Mpp, Msp, Msz, Mpz, zMss, zMpp, zMsp
-    
-    
+
+
+def linearize(expr, *subs_maps, perturb_var=eps):
+    expr_lin = expr
+    for subs_map in subs_maps:
+        expr_lin = expr_lin.subs(subs_map)
+    expr_lin = expr_lin.simplify().expand().coeff(perturb_var, 1)
+    return expr_lin
+
