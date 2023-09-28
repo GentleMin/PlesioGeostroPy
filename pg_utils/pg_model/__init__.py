89c56b104be511afd3797787f9a67303282ea709
gentlemin <40021781+GentleMin@users.noreply.github.com>
2023-09-28 17:52:59 +0200

    Files adjusted to fit new interface

--- pg_utils/pg_model/__init__.py
+++ pg_utils/pg_model/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 
 
-from .pg_fields import x, y, z, s, p, z, r, theta, t, n, m , H, H_s, omega
+# from .pg_fields import x, y, z, s, p, z, r, theta, t, n, m , H, H_s, omega
+from .core import x, y, z, s, p, z, r, theta, t, n, m , H, H_s, omega
--- pg_utils/pg_model/pg_forcing.py
+++ pg_utils/pg_model/pg_forcing.py
@@ -7,7 +7,7 @@ Jingtao Min @ ETH-EPM, 09.2023
 import sympy
 from sympy import diff
 from sympy import Derivative as diff_u
-from .pg_fields import *
+from .core import *
 from .base_utils import linearize
 
 
@@ -20,10 +20,17 @@ Lz_asym = sympy.Function(r"\widetilde{L_z}")(s, p, t)
 Le_p = sympy.Function(r"L_{e\phi}")(s, p, t)
 
 # Explicit expressions for Lorentz force
-Ls_sym_expr = 1/s*diff(s*Mss, s) + 1/s*diff(Msp, p) - Mpp/s + (Msz_p_expr - Msz_m_expr) + s/H*(Mss_p_expr + Mss_m_expr)
-Lp_sym_expr = 1/s*diff(s*Msp, s) + 1/s*diff(Mpp, p) + Msp/s + (Mpz_p_expr - Mpz_m_expr) + s/H*(Msp_p_expr + Msp_m_expr)
-Lz_asym_expr = 1/s*diff(s*Msz, s) + 1/s*diff(Mpz, p) + (Mzz_p_expr + Mzz_m_expr - 2*Bz_e*Bz_e) + s/H*(Msz_p_expr - Msz_m_expr)
-Le_p_expr = Bs_e*diff(Bp_e, s) + 1/s*Bp_e*diff(Bp_e, p) + Bz_e*dBp_dz_e + 1/s*Bs_e*Bp_e
+Ls_sym_expr = 1/s*diff(s*pgvar.Mss, s) + 1/s*diff(pgvar.Msp, p) - pgvar.Mpp/s \
+    + (pgvar.Bs_p*pgvar.Bz_p - pgvar.Bs_m*pgvar.Bz_m) \
+    + s/H*(pgvar.Bs_p*pgvar.Bs_p + pgvar.Bs_m*pgvar.Bs_m)
+Lp_sym_expr = 1/s*diff(s*pgvar.Msp, s) + 1/s*diff(pgvar.Mpp, p) + pgvar.Msp/s \
+    + (pgvar.Bp_p*pgvar.Bz_p - pgvar.Bp_m*pgvar.Bz_m) \
+    + s/H*(pgvar.Bs_p*pgvar.Bp_p + pgvar.Bs_m*pgvar.Bp_m)
+Lz_asym_expr = 1/s*diff(s*pgvar.Msz, s) + 1/s*diff(pgvar.Mpz, p) \
+    + (pgvar.Bz_p*pgvar.Bz_p + pgvar.Bz_m*pgvar.Bz_m - 2*pgvar.Bz_e*pgvar.Bz_e) \
+    + s/H*(pgvar.Bs_p*pgvar.Bz_p - pgvar.Bs_m*pgvar.Bz_m)
+Le_p_expr = pgvar.Bs_e*diff(pgvar.Bp_e, s) + 1/s*pgvar.Bp_e*diff(pgvar.Bp_e, p) \
+    + pgvar.Bz_e*pgvar.dBp_dz_e + 1/s*pgvar.Bs_e*pgvar.Bp_e
 
 
 
@@ -34,21 +41,21 @@ Le_p_expr = Bs_e*diff(Bp_e, s) + 1/s*Bp_e*diff(Bp_e, p) + Bz_e*dBp_dz_e + 1/s*Bs
 # Linearized form of Lorentz force in the equatorial plane $L_{e\phi}$
 # $L_{e\phi}$ is quadratic in the magnetic field components in the equatorial plane.
 # Linearized form involves cross terms between background and perturbational magnetic fields.
-Le_p_lin = linearize(Le_p_expr, linearization_subs_map, perturb_var=eps)
+Le_p_lin = linearize(Le_p_expr, pg_linmap, perturb_var=eps)
 
 # For the integrated quantities, the Lorentz force IS a linear function of magnetic moments. Essentially no linearization required.
 # However, the boundary terms and the equatorial terms are quadratic in magnetic fields. These terms need to be linearized.
 
 # Linearized form for $\overline{L_s}$
-Ls_sym_lin = linearize(Ls_sym_expr, linearization_subs_map, perturb_var=eps)
+Ls_sym_lin = linearize(Ls_sym_expr, pg_linmap, perturb_var=eps)
 
 # Linearized form for $\overline{L_\phi}$
-Lp_sym_lin = linearize(Lp_sym_expr, linearization_subs_map, perturb_var=eps)
+Lp_sym_lin = linearize(Lp_sym_expr, pg_linmap, perturb_var=eps)
 
 # Linearized form for $\widetilde{L_z}$
-Lz_asym_lin = linearize(Lz_asym_expr, linearization_subs_map, perturb_var=eps)
+Lz_asym_lin = linearize(Lz_asym_expr, pg_linmap, perturb_var=eps)
 
 # Curl of horizontal components $\nabla \times \mathbf{L}_e$
 # Curl is linear, linearize (curl (field)) = curl (linearize (field))
-curl_L = cyl_op.curl((Ls_sym_lin, Lp_sym_lin, 0))[2]
+curl_L = cyl.curl((Ls_sym_lin, Lp_sym_lin, 0))[2]
 
