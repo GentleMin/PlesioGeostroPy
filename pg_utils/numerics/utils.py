4261a06837b353d9948c5694abca8288bbcbdb19
gentlemin <40021781+GentleMin@users.noreply.github.com>
2023-10-15 23:38:00 +0200

    New file for numerical utilities; Malkus analytical solution

---
+++ pg_utils/numerics/utils.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+
+import numpy as np
+from typing import Union, List, Optional
+
+
+def eigenfreq_psi_op(m: Union[int, np.ndarray], n: Union[int, np.ndarray]):
+    """
+    """
+    return -m/(n*(2*n + 2*m + 1) + m/2 + m**2/4)
+
+
+def eigenfreq_Malkus(m: Union[int, np.ndarray], n: Union[int, np.ndarray], 
+    Le: float, mode: str="all", timescale: str="rotation"):
+    """
+    """
+    omega0 = eigenfreq_psi_op(m, n)
+    bg_field_mod = Le**2*(4*m*(m - omega0))/(omega0**2)
+    if timescale == "rotation":
+        prefactor = omega0/2
+    elif timescale == "alfven":
+        prefactor = omega0/2/Le
+    else:
+        raise AttributeError
+    if mode == "fast":
+        return prefactor*(1 + np.sqrt(1 + bg_field_mod))
+    elif mode == "slow":
+        return prefactor*(1 - np.sqrt(1 + bg_field_mod))
+    else:
+        return prefactor*(1 + np.sqrt(1 + bg_field_mod)), \
+            prefactor*(1 - np.sqrt(1 + bg_field_mod))
+
