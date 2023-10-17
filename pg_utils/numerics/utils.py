411196d935655c6ba1c98f7ea1d928d426578325
gentlemin <40021781+GentleMin@users.noreply.github.com>
2023-10-17 19:19:44 +0200

    3D theoretical solution for Malkus eigenmodes

--- pg_utils/numerics/utils.py
+++ pg_utils/numerics/utils.py
@@ -11,11 +11,38 @@ def eigenfreq_psi_op(m: Union[int, np.ndarray], n: Union[int, np.ndarray]):
     return -m/(n*(2*n + 2*m + 1) + m/2 + m**2/4)
 
 
-def eigenfreq_Malkus(m: Union[int, np.ndarray], n: Union[int, np.ndarray], 
+def eigenfreq_inertial3d(m: Union[int, np.ndarray], n: Union[int, np.ndarray]):
+    """
+    """
+    return -2/(m+2)*(np.sqrt(1 + m*(m+2)/n/(2*n+2*m+1)) - 1)
+
+
+def eigenfreq_Malkus_pg(m: Union[int, np.ndarray], n: Union[int, np.ndarray], 
     Le: float, mode: str="all", timescale: str="rotation"):
     """
     """
     omega0 = eigenfreq_psi_op(m, n)
+    bg_field_mod = Le**2*(4*m*(m - omega0))/(omega0**2)
+    if timescale == "rotation":
+        prefactor = omega0/2
+    elif timescale == "alfven":
+        prefactor = omega0/2/Le
+    else:
+        raise AttributeError
+    if mode == "fast":
+        return prefactor*(1 + np.sqrt(1 + bg_field_mod))
+    elif mode == "slow":
+        return prefactor*(1 - np.sqrt(1 + bg_field_mod))
+    else:
+        return prefactor*(1 + np.sqrt(1 + bg_field_mod)), \
+            prefactor*(1 - np.sqrt(1 + bg_field_mod))
+
+
+def eigenfreq_Malkus_3d(m: Union[int, np.ndarray], n: Union[int, np.ndarray], 
+    Le: float, mode: str="all", timescale: str="rotation"):
+    """
+    """
+    omega0 = eigenfreq_inertial3d(m, n)
     bg_field_mod = Le**2*(4*m*(m - omega0))/(omega0**2)
     if timescale == "rotation":
         prefactor = omega0/2
