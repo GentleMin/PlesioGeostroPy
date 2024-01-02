3d7993ee8bf1dca38cfffdc5ec94ecdca6bd0aeb
gentlemin <40021781+GentleMin@users.noreply.github.com>
2024-01-03 00:52:31 +0100

    Setup scripts and utilities for eigenvalue problem

---
+++ script_eigen_solve.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""Script for solving the eigenvalue problem in axisymmetric background field
+"""
+
+
+from pg_utils import eigen
+import os
+output_dir = "./out/eigen/"
+
+
+from pg_utils.pg_model import bg_fields
+
+# eigen.form_equations(
+#     eq_mode="reduced",
+#     components=["Lorentz"],
+#     timescale="Alfven",
+#     bg=bg_fields.BackgroundToroidalQuadrupole(),
+#     save_to=os.path.join(output_dir, "Toroidal_Quadrupole/eqs_reduced.json"),
+#     verbose=5
+# )
+
+
+# from pg_utils.pg_model import expand_conjugate as xpd_cfg
+# import numpy as np
+
+# eigen.collect_matrix_elements(
+#     read_from=os.path.join(output_dir, "Toroidal_Quadrupole/eqs_cg.json"),
+#     manual_select=xpd_cfg.field_indexer,
+#     expansion_recipe=xpd_cfg.recipe,
+#     save_to=os.path.join(output_dir, "Toroidal_Quadrupole/Transformed/matrix_expr.json"),
+#     verbose=5
+# )
+
+
+from pg_utils.pg_model import expand_stream_force as xpd_cfg
+from pg_utils.pg_model import params, expansion
+import sympy
+
+# # Malkus field / Hydrodynamic modes
+# parameters = {
+#     params.Le: sympy.Rational(1, 10000),
+#     expansion.m: sympy.Integer(3)
+# }
+# Toroidal quadrupolar field
+parameters = {
+    bg_fields.BackgroundToroidalQuadrupole().params[0]: 3*sympy.sqrt(3)/2,
+    params.Le: sympy.sqrt(2)*sympy.Rational(1, 10000),
+    expansion.m: sympy.Integer(3)
+}
+
+# eigen.compute_matrix_numerics(
+#     read_from=os.path.join(output_dir, 
+#         "Toroidal_Quadrupole/Reduced/matrix_expr.json"),
+#     xpd_recipe=xpd_cfg.recipe,
+#     Ntrunc=50,
+#     par_val=parameters,
+#     save_to=os.path.join(output_dir, 
+#         "Toroidal_Quadrupole/Reduced/matrix_m3_N50.h5"),
+#     verbose=5
+# )
+
+eigen.compute_eigen(
+    read_from=os.path.join(output_dir, 
+        "Toroidal_Quadrupole/Reduced/matrix_m3_N50.h5"),
+    save_to=os.path.join(output_dir, 
+        "Toroidal_Quadrupole/Reduced/eigen_m3_N50.h5"),
+    verbose=5
+)
