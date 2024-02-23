Correct multi-precision conversions; correct multi-precision quadrature

pg_utils/numerics/matrices.py
@@ -133,11 +133,15 @@ def quad_matrix_mpmath(operand_A: sympy.Expr, operand_B: sympy.Expr,
     ]
     f_A = sympy.lambdify([n_test, xi], operand_A.doit(), modules=lambdify_modules)
     f_B = sympy.lambdify([n_trial, xi], operand_B.doit(), modules=lambdify_modules)
-    Ntest, Xi_test = np.meshgrid(nrange_A, xi_quad, indexing='ij')
-    Phi_A = f_A(Ntest, Xi_test)
-    Ntrial, Xi_trial = np.meshgrid(nrange_B, xi_quad, indexing='ij')
-    Phi_B = f_B(Ntrial, Xi_trial)
-    return (Phi_A*wt_quad) @ Phi_B.T
+    
+    with mp.workdps(n_dps):
+        Ntest, Xi_test = np.meshgrid(nrange_A, xi_quad, indexing='ij')
+        Phi_A = f_A(Ntest, Xi_test)
+        Ntrial, Xi_trial = np.meshgrid(nrange_B, xi_quad, indexing='ij')
+        Phi_B = f_B(Ntrial, Xi_trial)
+        quad_matrix = (Phi_A*wt_quad) @ Phi_B.T
+    
+    return quad_matrix
 
 
 def quad_matrix_gmpy2(operand_A: sympy.Expr, operand_B: sympy.Expr, 
@@ -168,22 +172,31 @@ def quad_matrix_gmpy2(operand_A: sympy.Expr, operand_B: sympy.Expr,
     """
     lambdify_funcs = [{
         "jacobi": functools.partial(special.eval_jacobi_recur_mp, dps=n_dps, backend="gmpy2"), 
+        # "jacobi": lambda Nmesh, a, b, zmesh: special.eval_jacobi_recur_mp(Nmesh, a, b, zmesh, dps=32, backend="gmpy2"),
         **symparser.v_functions_gmpy2
     }]
     gmpy2_printer=symparser.Gmpy2Printer(settings={
         'fully_qualified_modules': False, 
         'inline': True, 
         'allow_unknown_functions': True, 
-        'user_functions': {"jacobi": "jacobi", "sqrt": "sqrt"}}, prec=112)
+        'user_functions': {"jacobi": "jacobi", "sqrt": "sqrt"}
+        }, prec=112)
+    
     f_A = sympy.lambdify([n_test, xi], operand_A.doit(), 
         modules=lambdify_funcs, printer=gmpy2_printer)
     f_B = sympy.lambdify([n_trial, xi], operand_B.doit(), 
         modules=lambdify_funcs, printer=gmpy2_printer)
-    Ntest, Xi_test = np.meshgrid(nrange_A, xi_quad, indexing='ij')
-    Phi_A = f_A(Ntest, Xi_test)
-    Ntrial, Xi_trial = np.meshgrid(nrange_B, xi_quad, indexing='ij')
-    Phi_B = f_B(Ntrial, Xi_trial)
-    return (Phi_A*wt_quad) @ Phi_B.T
+    
+    _, target_prec = utils.transform_dps_prec(dps=n_dps)
+    with gp.local_context(gp.context(), precision=target_prec) as ctx:
+        
+        Ntest, Xi_test = np.meshgrid(nrange_A, xi_quad, indexing='ij')
+        Phi_A = f_A(Ntest, Xi_test)
+        Ntrial, Xi_trial = np.meshgrid(nrange_B, xi_quad, indexing='ij')
+        Phi_B = f_B(Ntrial, Xi_trial)
+        quad_matrix = (Phi_A*wt_quad) @ Phi_B.T
+    
+    return quad_matrix
 
 
 class InnerQuad_Rule:
@@ -425,11 +438,11 @@ class InnerQuad_GaussJacobi(InnerQuad_Rule):
         
         # Throw warning of singularity if alpha or beta <= -1
         if alpha <= -1:
-            alpha = 0
+            alpha = sympy.S.Zero
             quadN *= 2
             warnings.warn("Endpoint singularity detected! Check for integrability!")
         if beta <= -1:
-            beta = 0
+            beta = sympy.S.Zero
             quadN *= 2
             warnings.warn("Endpoint singularity detected! Check for integrability!")
         
@@ -555,6 +568,7 @@ class InnerQuad_GaussJacobi(InnerQuad_Rule):
             beta_mp = mp.mpf(str(beta.evalf(n_dps)))
         root_result = special.roots_jacobi_mp(int(quad_N), alpha_mp, beta_mp, n_dps=n_dps)
         xi_quad, wt_quad = root_result.xi, root_result.wt
+        # mp.nprint(xi_quad[0], 32)
         return quad_matrix_mpmath(opd_A, opd_B, nrange_test, nrange_trial, 
             xi_quad, wt_quad, n_dps=n_dps)
         

pg_utils/numerics/utils.py
@@ -6,7 +6,7 @@
 import numpy as np
 import gmpy2 as gp
 import mpmath as mp
-from typing import Union, List, Optional, Callable, Any
+from typing import Union, List, Optional, Callable, Any, Literal
 from scipy.sparse import coo_array
 
 
@@ -153,7 +153,8 @@ def to_gpmy2_f(x: np.ndarray, dps: Optional[int] = None,
     if dps is None and prec is None:
         return np.vectorize(lambda x: gp.mpfr(str(x)), otypes=(object,))(x)
     _, prec_target = transform_dps_prec(dps=dps, prec=prec)
-    return np.vectorize(lambda x: gp.mpfr(str(x), prec_target), otypes=(object,))(x)
+    with mp.workprec(prec_target):
+        return np.vectorize(lambda x: gp.mpfr(str(x), prec_target), otypes=(object,))(x)
 
 def to_mpmath_f(x: np.ndarray, dps: Optional[int] = None, 
     prec: Optional[int] = None) -> np.ndarray:
@@ -179,10 +180,11 @@ def to_gpmy2_c(x: np.ndarray, dps: Optional[int] = None,
             lambda x: gp.mpc(gp.mpfr(str(x.real)), imag=gp.mpfr(str(x.imag))), 
             otypes=(object,))(x)
     _, prec_target = transform_dps_prec(dps=dps, prec=prec)
-    return np.vectorize(
-        lambda x: gp.mpc(gp.mpfr(str(x.real)), imag=gp.mpfr(str(x.imag)), 
-                         precision=prec_target), 
-        otypes=(object,))(x)
+    with mp.workprec(prec_target):
+        return np.vectorize(
+            lambda x: gp.mpc(gp.mpfr(str(x.real), prec_target), imag=gp.mpfr(str(x.imag), prec_target), 
+                            precision=prec_target), 
+            otypes=(object,))(x)
 
 def to_mpmath_c(x: np.ndarray, dps: Optional[int] = None, 
     prec: Optional[int] = None) -> np.ndarray:
@@ -245,3 +247,19 @@ def allclose_sparse(array_1, array_2, rtol=1e-5, atol=1e-8):
     c = np.abs(array_1 - array_2) - rtol * np.abs(array_2)
     return c.max() <= atol
 
+
+def to_dense_obj(obj_array: coo_array, fill_zero: Any):
+    """Convert a sparse object array to dense form
+    """
+    dense_array = np.full(obj_array.shape, fill_zero, dtype=object)
+    for i_data, data in enumerate(obj_array.data):
+        dense_array[obj_array.row[i_data], obj_array.col[i_data]] = data
+    return dense_array
+
+
+def to_dense_gmpy2(gmpy2_array: coo_array, prec: int, 
+    mode: Literal['c', 'f'] = 'c') -> np.ndarray:
+    """Convert a sparse gmpy2 array to dense form
+    """
+    gmpy2_zero = gp.mpfr('0.0', prec) if mode == 'f' else gp.mpc('0.0+0.0j', prec)
+    return to_dense_obj(gmpy2_array, gmpy2_zero)